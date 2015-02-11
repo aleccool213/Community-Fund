87b2b4f6ee643b8140d2fe661ad0bf2aae105d1f
Timo Vink <timovink@gmail.com>
2015-02-11 15:24:41 -0500

    Add admin property to users.

--- CommunityFund/db/schema.rb
+++ CommunityFund/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150203163200) do
+ActiveRecord::Schema.define(version: 20150211200457) do
 
   create_table "communities", force: true do |t|
     t.string   "name"
@@ -33,12 +33,12 @@ ActiveRecord::Schema.define(version: 20150203163200) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20150203163200) do
     t.integer  "project_id"
     t.string   "hometown"
     t.string   "homestate"
+    t.boolean  "admin",                  default: false, null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
--- CommunityFund/db/seeds.rb
+++ CommunityFund/db/seeds.rb
@@ -1,5 +1,11 @@
+Community.destroy_all
 Community.create(name: "Science and Technology", description: "", active: true)
 Community.create(name: "Automotive", description: "", active: true)
 Community.create(name: "Sports", description: "", active: true)
 Community.create(name: "Health", description: "", active: true)
 Community.create(name: "Education", description: "", active: true)
+
+User.destroy_all
+User.create(username: "administrator", email: "admin@community-fund.ca",
+            password: "password1-", password_confirmation: "password1-",
+						admin: true)
