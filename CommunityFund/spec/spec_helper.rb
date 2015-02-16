bbea177e124d935481909a9814f12e0162fdc9b6
Timo Vink <timovink@gmail.com>
2015-02-16 13:05:25 -0500

    Set up the FactoryGirl and Devise helpers for RSpec.

--- CommunityFund/spec/spec_helper.rb
+++ CommunityFund/spec/spec_helper.rb
@@ -15,7 +15,7 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
-require 'factory_girl'
+require 'factory_girl_rails'
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
