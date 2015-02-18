fd4fa0bd5d4c70da94186be586c4ee30cb096804
ChrisSandison <chris.sandison@gmail.com>
2015-02-18 18:22:48 -0500

    Fixing typo, adding comment

--- CommunityFund/lib/project_closer.rb
+++ CommunityFund/lib/project_closer.rb
@@ -1,9 +1,10 @@
 class ProjectCloser
   # TODO close out all projects whose daedlines have been met. This includes changing their activity status, sending out review notifications or whatever the hell you want
+  # TODO test this
   def self.close
     Project.active.where("completion_date < ?", DateTime.now).find_each(batch_size: 100) do |project|
       # TODO - just call update(open: false)? There has to be a better way of doing this
-      project.open =f alse
+      project.open = false
       # TODO send notifications to gather feedback from user
 
       project.save
