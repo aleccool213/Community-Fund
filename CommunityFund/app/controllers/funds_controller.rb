3669c91befe196b1c0b519041295f636d1798a11
ChrisSandison <chris.sandison@gmail.com>
2015-02-20 13:03:33 -0500

    Finished new fund page, added validators for amount and rewards

--- CommunityFund/app/controllers/funds_controller.rb
+++ CommunityFund/app/controllers/funds_controller.rb
@@ -7,11 +7,18 @@ class FundsController < ApplicationController
 
     # load communities for this project
     # if user is in all of them, don't load any
+    if current_user.communities == @project.communities
+      @communities = []
     # if user is in no communities, load all project communities
+    elsif current_user.communities.blank?
+      @communities = @project.communities
     # otherwise only load communities that user is not in
+    else
+      @communities = @project.communities.where("id not in (?)", current_user.communities.pluck(:id))
+    end
   end
 
   def create
-
+    binding.pry
   end
 end
--- CommunityFund/app/models/fund.rb
+++ CommunityFund/app/models/fund.rb
@@ -3,7 +3,4 @@ class Fund < ActiveRecord::Base
   belongs_to :project
   has_one :reward
 
-  def self.minimum_amount
-    10
-  end
 end
