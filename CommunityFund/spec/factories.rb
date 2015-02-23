559169f7763b905f82fdef4eeccc46985134197e
Timo Vink <timovink@gmail.com>
2015-02-23 14:22:06 -0500

    Fix factories to delay evaluation.

--- CommunityFund/spec/factories.rb
+++ CommunityFund/spec/factories.rb
@@ -1,10 +1,9 @@
-FactoryGirl.define do  
-
+FactoryGirl.define do
 	factory :user do
 		sequence(:username)		{ |n| Faker::Internet.user_name }
 		sequence(:email)		{ |n| Faker::Internet.email }
-		password				"password1-"
-		password_confirmation	"password1-"
+		password				'password1-'
+		password_confirmation	'password1-'
 
 		factory :admin do
 			admin true
@@ -18,38 +17,37 @@ FactoryGirl.define do
 	end
 
 	factory :reward do
-		reward_level      100
+		reward_level	  100
 		sequence(:description)	{ |n| Faker::Company.catch_phrase}
 	end
 
 	factory :project do
 		sequence(:name)			{ |n| Faker::Commerce.product_name }
 		sequence(:description)	{ |n| Faker::Lorem.paragraph }
-		target_amount				1600
-		completion_date			DateTime.now + 1.month
-		communities					[ FactoryGirl.create(:community) ]
-		initiator_id				FactoryGirl.create(:user).id
-		open                true
+		target_amount			1600
+		completion_date			{ DateTime.now + 1.month }
+		communities				{ [ FactoryGirl.create(:community) ] }
+		initiator_id			{ FactoryGirl.create(:user).id }
+		open					true
 
 		trait :with_rewards do
-			rewards							[ FactoryGirl.create(:reward), FactoryGirl.create(:reward)]
+			rewards				{ [FactoryGirl.create(:reward), FactoryGirl.create(:reward)] }
 		end
-	
+
 	end
 
 	factory :fund do
-		user  							FactoryGirl.create(:user)
-  	project							FactoryGirl.create(:project, :with_rewards)					
-    amount            	100
-  end
+		user  					{ FactoryGirl.create(:user) }
+  		project					{ FactoryGirl.create(:project, :with_rewards) }
+		amount					100
+  	end
 
-  factory :feedback do
-    rating						0
-    submitted   			false
-    dismissed					false
-    description 			"Project was great"
-    user_id 					FactoryGirl.create(:user).id
-    project_id     		FactoryGirl.create(:project).id  
-  end
-
+	factory :feedback do
+		rating					0
+		submitted   			false
+		dismissed				false
+		description 			'Project was great'
+		user_id 				{ FactoryGirl.create(:user).id }
+		project_id	 			{ FactoryGirl.create(:project).id }
+	end
 end
