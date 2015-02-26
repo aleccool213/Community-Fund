1842d85092229d9d493bd6d12ee1f758fe9829e2
bensonchan <bensonyk.chan@mail.utoronto.ca>
2015-02-26 16:42:03 -0500

    Added native avatar support

--- CommunityFund/app/helpers/application_helper.rb
+++ CommunityFund/app/helpers/application_helper.rb
@@ -1,8 +1,11 @@
 module ApplicationHelper
   def avatar_url(user, size = 80)
-    # TODO Add custom default avatar.
     default_url = "mm"  #"#{root_url}images/guest.png"
-    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=#{CGI.escape(default_url)}"
+    begin
+      Avatar.find(user.id).avatar
+    rescue
+      gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+      "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=#{CGI.escape(default_url)}"
+    end
   end
 end
