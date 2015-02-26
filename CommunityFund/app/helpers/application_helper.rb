c7fe6d0887e3fbf7451a5ad7d36cdc5769440bab
bensonchan <bensonyk.chan@mail.utoronto.ca>
2015-02-26 13:45:35 -0500

    Added profile pictures

--- CommunityFund/app/helpers/application_helper.rb
+++ CommunityFund/app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def avatar_url(user, size = 80)
+    # TODO Add custom default avatar.
+    default_url = "mm"  #"#{root_url}images/guest.png"
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=#{CGI.escape(default_url)}"
+  end
 end
