d3d26bc5011034284b3049c886e9d940a54b933f
bensonchan <bensonyk.chan@mail.utoronto.ca>
2015-02-26 14:14:20 -0500

    Updated user profiles to include new features

--- CommunityFund/app/controllers/users_controller.rb
+++ CommunityFund/app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by_username(params[:username])
+    @projects = @user.projects
+    @funds = @user.funds
+    @communities = @user.communities
+    @feedbacks = @user.feedbacks
   end
 end
