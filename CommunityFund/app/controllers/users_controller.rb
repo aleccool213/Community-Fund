d713e4832cd78caaef4f78441332631e6ebc2713
bensonchan <bensonyk.chan@mail.utoronto.ca>
2015-03-04 17:32:44 -0500

    Added reward descriptions and sorted projects/funds/feedbacks

--- CommunityFund/app/controllers/users_controller.rb
+++ CommunityFund/app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
   def show
     if @user = User.find_by_username(params[:username])
-      @projects = @user.projects
-      @funds = @user.funds
+      @projects = @user.projects.order('updated_at DESC')
+      @funds = @user.funds.order('updated_at DESC')
       @communities = @user.communities.active
-      @feedbacks = @user.feedbacks
+      @feedbacks = @user.feedbacks.order('updated_at DESC')
     else
       redirect_to root_path
     end
--- CommunityFund/app/helpers/users_helper.rb
+++ CommunityFund/app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def get_reward(fund)
+    if r = Reward.find_by_id(fund.reward_id)
+      "Reward: #{r.description}"
+    else
+      ""
+    end
+  end
 end
