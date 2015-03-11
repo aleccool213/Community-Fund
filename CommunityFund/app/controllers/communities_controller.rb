4525d7c1561056debd636a2c4dc40f9e8af207d8
ChrisSandison <chris.sandison@gmail.com>
2015-03-11 14:20:21 -0400

    Made communities publicly viewable, changed some styles

--- CommunityFund/app/controllers/communities_controller.rb
+++ CommunityFund/app/controllers/communities_controller.rb
@@ -1,5 +1,5 @@
 class CommunitiesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show]
   
   def index
     @communities = Community.active
