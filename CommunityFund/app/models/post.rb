7c50c33be443204b3a73dec8af26e205b8b7f46e
ChrisSandison <chris.sandison@gmail.com>
2015-03-24 16:07:52 -0400

    Small fix

--- CommunityFund/app/models/post.rb
+++ CommunityFund/app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ActiveRecord::Base
   def title_text
     obj = get_object
     text = "#{self.user.username.capitalize} "
-    text += "(Initiator) " if obj.class == "Project" && obj.is_initiator?(self.user)
+    text += "(Initiator) " if obj.class == Project && obj.is_initiator?(self.user)
     text += "(Administrator) " if self.user.admin?
     text += "(Funder) " if obj.class == "Project" && Fund.where(project_id: obj.id, user_id: self.user.id).present?
     text
