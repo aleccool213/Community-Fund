e63521ee47ace8490d58baf4ea738f516134cf42
ChrisSandison <chris.sandison@gmail.com>
2015-03-31 13:14:07 -0400

    Added ability to delete posts by poster or by administrator. Did some refactoring on the posts controller as well

--- CommunityFund/app/controllers/posts_controller.rb
+++ CommunityFund/app/controllers/posts_controller.rb
@@ -3,16 +3,13 @@ class PostsController < ApplicationController
 
   def create
     if params[:content].present?
-      post = Post.new
-      post.user = current_user
-      post.send("#{params[:object_type]}_id=", params[:object_id])
-      post.content = params[:content]
-      post.save
-
-      @type = params[:object_type]
-      @id = params[:object_id]
-
-      render json: {status: :success, content: params[:content], url: post.user.avatar_url(70), username: post.title_text,  userpath: "/users/#{post.user.username}", timestamp: post.updated_at.strftime("%m/%d/%Y at %I:%M%p")}
+      @post = Post.new
+      @post.user = current_user
+      @post.send("#{params[:object_type]}_id=", params[:object_id])
+      @post.content = params[:content]
+      @post.save
+      
+      render json: {status: :success, html: render_to_string("posts/single_post", layout: false)}
     else
       render json: {status: :failure}
     end
@@ -25,6 +22,20 @@ class PostsController < ApplicationController
     @posts = type.capitalize.constantize.find_by_id(id).posts.paginate(page: page, per_page: 15).order("updated_at DESC")
     if request.xhr?
       render json: {success: true, html: render_to_string("index", layout: false)}
+    end
+  end
+
+  def destroy
+    type = params[:object_type]
+    id = params[:id]
+    object_id = params[:object_id]
+    @post = type.capitalize.constantize.find_by_id(object_id).posts.find_by_id(id)
+
+    if @post.present?
+      @post.destroy
+      render json: {success: true}
+    else
+      render json: {status: :failure}
     end
   end
 end
