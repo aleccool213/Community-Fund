756a7995af3cc1d6fd28729d0cea4fc249cf12ee
bensonchan <bensonyk.chan@mail.utoronto.ca>
2015-03-31 20:52:27 -0400

    Added creating accounts using facebook

--- CommunityFund/app/controllers/omniauth_callbacks_controller.rb
+++ CommunityFund/app/controllers/omniauth_callbacks_controller.rb
@@ -1,9 +1,13 @@
-class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
+    
+    # Debug output
+    puts request.env["omniauth.auth"]
+    puts @user.errors.to_a
 
     if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication  # this will throw if @user is not activated
+      sign_in_and_redirect @user
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
--- CommunityFund/app/models/user.rb
+++ CommunityFund/app/models/user.rb
@@ -4,11 +4,12 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable,
          :validatable, :timeoutable, :omniauthable,
-         :omniauth_providers => [:facebook, :twitter]
+         omniauth_providers: [:facebook]
 
   validates :username, presence: true, uniqueness: true,
             format: { with: /\A[A-Za-z0-9_]+\z/,
                       message: "Only alphanumerical characters and underscores allowed." }
+  
 
   has_and_belongs_to_many :communities
   has_many :projects
@@ -66,9 +67,17 @@ class User < ActiveRecord::Base
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      user.username = auth.info.nickname
+      user.username = auth.info.first_name
       user.first_name = auth.info.first_name
       user.last_name = auth.info.last_name
+    end
+  end
+  
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
     end
   end
 end
